6a979ad13fc18c4232864c7ae30eab9818421c84
arusoghomonyan <soghomonyan1460@gmail.com>
2025-04-07 20:52:02 +0400

    Create 9bitwise.c

---
+++ 9bitwise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+   
+   int main(){
+   
+     int num;
+     int index = 0;
+   
+     printf("Enter the number: ");
+     scanf("%d", &num);
+  
+    printf("Enter the index: ");
+    scanf("%d", &index);
+  
+    num = num & ~(1 << index);
+    printf("%d", num);
+    return 0;
+  }
