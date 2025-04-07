2858a0ec2f741d33394009ea89542f1cd12c127e
arusoghomonyan <soghomonyan1460@gmail.com>
2025-04-07 20:39:54 +0400

    Create 5bitwise.c

---
+++ 5bitwise.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+   
+   int main(){
+     int result;
+     int num;
+     printf("Enter the positive number: ");
+     scanf("%d", &num);
+     result = num << 1;
+     printf("%d is the multiple of positive number", result);
+    return 0;
+  }                    
