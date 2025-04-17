66d7fb317360ab42c68234c20ea18e0a3c6ca181
arusoghomonyan <soghomonyan1460@gmail.com>
2025-04-17 22:39:22 +0400

    Create matrix2.c

---
+++ matrix2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+		int arr[3][3];
+
+		for(int row = 0; row < 3; ++row)
+		{
+				for(int col = 0; col < 3; ++col){
+						scanf("%d", &arr[row][col]);
+				}
+		}
+		int sum = 0;
+		for(int row = 0; row < 3; ++row){
+				for(int col = 0; col < 3; ++col){
+						if(row == col){
+								sum += arr[row][col];
+						}	 
+				}
+		}
+		printf("%d", sum);
+	return 0;
+}
